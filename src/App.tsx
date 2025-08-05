fbd7fb29b64d12b0be32d318f4216c059754daf0
cc004 <carboncookie004@protonmail.com>
2025-08-05 19:19:59 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,9 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        basename={"/graphite-glow-page/"}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
